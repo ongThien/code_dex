364665ef9adf75e57a3daac5dbc8fe4ba61bab94
ongthien <ongthiendev@gmail.com>
2024-03-14 14:56:58 +0700

    remove unneeded files

--- python_proj/wiki.py
+++
@@ -1,9 +0,0 @@
-import wikipedia
-
-i = wikipedia.search("Philosophy of life")
-j = wikipedia.summary("Python (programming language)")
-k = wikipedia.search("Galaxy")
-
-print(i)
-print(j)
-print(k)
